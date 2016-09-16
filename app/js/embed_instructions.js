4bba67cb77458348e01c9a8fe8d455bfa1a99a4d
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-09-16 14:20:48 -0700

    tweak embed instructions to fix black text

--- app/js/embed_instructions.js
+++ app/js/embed_instructions.js
@@ -16,7 +16,7 @@ var iframe = document.querySelectorAll('iframe');
 code[0].textContent = '<iframe src="'+url+'" allowtransparency="1" frameborder="0"></iframe>';
 code[3].textContent = '<iframe src="'+url+'?disclosureColor=ff8800&hueShift=120" allowtransparency="1" frameborder="0"></iframe>';
 
-iframe[0].src = iframeUrl;
-iframe[1].src = iframeUrl;
+iframe[0].src = iframeUrl+'?disclosureColor=ffffff';
+iframe[1].src = iframeUrl+'?disclosureColor=ffffff';
 iframe[2].src = iframeUrl+'?disclosureColor=ff8800&hueShift=120';
 
