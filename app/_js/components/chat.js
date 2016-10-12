50dfedeb1f25b0eb821be612bdd344317faa06bf
Josh Levinger <josh@levinger.net>
2016-10-12 15:19:00 -0700

    rip out fb pixel, use url params instead

--- app/_js/components/chat.js
+++ app/_js/components/chat.js
@@ -124,7 +124,6 @@ window.components.chat = function (doc, win) {
     submission.setRequestHeader("Content-Type", "application/json");
     submission.send(JSON.stringify(data));
     if (_paq) { _paq.push(['trackGoal', 1]); }
-    if (fbq) { fbq('track', 'CompleteRegistration'); }
 
     setTimeout(function() {
       bubble('bot', l10n['CHECK_YOUR_PHONE']);
